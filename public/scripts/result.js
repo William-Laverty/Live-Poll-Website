781ab4c7d09dec89aacb8414a0eee31263c99914
William Laverty <williamlaverty@williams-mbp.lan>
2024-06-11 20:09:09 +1000

    chore: Update chart.js dependency to version 4.4.3

--- public/scripts/result.js
+++ public/scripts/result.js
@@ -1,84 +1,61 @@
-document.addEventListener('DOMContentLoaded', async function() {
-  const event = localStorage.getItem('currentEvent');
-  if (event) {
-      document.getElementById('currentEvent').textContent = `${event}`;
-  } else {
-      window.location.href = 'index.html';
-      return; // Stop further execution if redirected
-  }
-
-  // Fetch the votes data from the server
-  try {
-      const response = await fetch(`/poll-options/${event}`);
-      const result = await response.json();
-
-      if (result.success) {
-          // Extract the option names and vote counts
-          const options = result.options;
-          const labels = options.map(option => option.optionId);
-          const voteCounts = options.map(option => option.votes);
-
-          // Update the chart with the fetched data
-          updateChart(labels, voteCounts);
-      } else {
-          console.error('Failed to fetch poll options:', result.message);
-      }
-  } catch (error) {
-      console.error('Error fetching poll options:', error);
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    const event = localStorage.getItem('currentEvent');
+    if (event) {
+        document.getElementById('currentEvent').textContent = `${event}`;
+    } else {
+        window.location.href = 'index.html';
+    }
 });
 
-function updateChart(labels, voteCounts) {
-  const ctx = document.getElementById('results');
+const ctx = document.getElementById('results');
 
-  new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: labels,
-          datasets: [{
-              label: 'Votes',
-              data: voteCounts,
-              backgroundColor: [
-                  'rgba(230, 60, 45, 0.8)',
-                  'rgba(252, 204, 0, 0.9)',
-                  'rgba(33, 59, 94, 0.9)',
-                  'rgba(136, 36, 38, 0.9)',
-                  'rgba(60, 155, 209, 0.9)',
-                  'rgba(92, 57, 111, 0.9)',
-                  'rgba(13, 8, 2, 0.9)',
-                  'rgba(26, 86, 48, 0.9)',
-                  'rgba(167, 166, 164, 0.9)',
-                  'rgba(29, 182, 120, 0.9)'
-              ],
-              borderColor: [
-                  'rgba(184, 48, 36, 1)',
-                  'rgba(202, 163, 0, 1)',
-                  'rgba(26, 47, 75, 1)',
-                  'rgba(109, 29, 30, 1)',
-                  'rgba(48, 124, 167, 1)',
-                  'rgba(74, 46, 89, 1)',
-                  'rgba(10, 6, 2, 1)',
-                  'rgba(20, 68, 38, 1)',
-                  'rgba(134, 133, 131, 1)',
-                  'rgba(23, 145, 96, 1)'
-              ],
-              borderWidth: 1,
-              borderRadius: 5
-          }]
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Blaxland', 'Burgmann', 'Eddison', 'Edwards', 'Garnsey', 'Garran', 'Hay', 'Jones', 'Sheaffe', 'Middleton'],
+    datasets: [{
+      label: '',
+      data: [12, 19, 3, 5, 2, 3, 5, 10, 12, 18],
+      backgroundColor: [
+        'rgba(230, 60, 45, 0.8)',
+        'rgba(252, 204, 0, 0.9)',
+        'rgba(33, 59, 94, 0.9)',
+        'rgba(136, 36, 38, 0.9)',
+        'rgba(60, 155, 209, 0.9)',
+        'rgba(92, 57, 111, 0.9)',
+        'rgba(13, 8, 2, 0.9)',
+        'rgba(26, 86, 48, 0.9)',
+        'rgba(167, 166, 164, 0.9)',
+        'rgba(29, 182, 120, 0.9)'
+      ],
+      borderColor: [
+        'rgba(184, 48, 36, 1)',
+        'rgba(202, 163, 0, 1)',
+        'rgba(26, 47, 75, 1)',
+        'rgba(109, 29, 30, 1)',
+        'rgba(48, 124, 167, 1)',
+        'rgba(74, 46, 89, 1)',
+        'rgba(10, 6, 2, 1)',
+        'rgba(20, 68, 38, 1)',
+        'rgba(134, 133, 131, 1)',
+        'rgba(23, 145, 96, 1)'
+      ],
+      borderWidth: 1,
+      borderRadius: 5
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+        legend: {
+          display: false,
+        }
+    },
+    scales: {
+      y: {
+        beginAtZero: true
       },
-      options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          plugins: {
-              legend: {
-                  display: false,
-              }
-          },
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
-      }
-  });
-}
+    }
+  }
+});
